713a25b0deb29115846c6ad9a5d57dd299c1311d
porpytd <porpy.finance@gmail.com>
2023-01-27 04:35:31 +0700

    myResume

---
+++ assets/scroll.js
@@ -0,0 +1,18 @@
+const sections = document.querySelectorAll("section");
+const navLi = document.querySelectorAll("header .nav-menu ul li a");
+window.onscroll = () => {
+  var current = "";
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    if (pageYOffset >= sectionTop - 60) {
+      current = section.getAttribute("id"); }
+  });
+
+  navLi.forEach((a) => {
+    a.classList.remove("active");
+    if (a.classList.contains(current)) {
+      a.classList.add("active");
+    }
+  });
+};
